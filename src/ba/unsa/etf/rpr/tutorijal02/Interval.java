7a0767defae0d9ddcaa38f9b2a7ee1810d62b62e
Admir Mehmedagic <amehmedagi1@etf.unsa.ba>
2020-11-02 14:19:58 +0100

    Prepravke metode isIn, i dodavanje preklapanja equals

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -19,10 +19,11 @@ boolean pocetnapripada, krajnjapripada;
         krajnjapripada = false;
     }
     public String toString(){
+        if(this.pocetna==0 && this.krajnja==0 && this.pocetnapripada==false && this.krajnjapripada== false) return ("()");
         if(this.pocetnapripada == true && this.krajnjapripada == true) return ("[" + this.pocetna + "," + this.krajnja + "]");
         if(this.pocetnapripada == true && this.krajnjapripada == false) return ("[" + this.pocetna + "," + this.krajnja + ")");
         if(this.pocetnapripada == false && this.krajnjapripada == true) return ("(" + this.pocetna + "," + this.krajnja + "]");
-        return ("(" + this.pocetna + "." + this.krajnja + ")");
+        return ("(" + this.pocetna + "," + this.krajnja + ")");
     }
 
     boolean isNull(){
@@ -31,7 +32,9 @@ boolean pocetnapripada, krajnjapripada;
     }
 
     boolean isIn(double tacka){
-        if(tacka >= this.pocetna && tacka <= this.krajnja) return true;
+        if(tacka > this.pocetna && tacka < this.krajnja) return true;
+        if(tacka == this.pocetna && this.pocetnapripada) return true;
+        if(tacka == this.krajnja && this.krajnjapripada) return true;
         return false;
     }
 
@@ -89,5 +92,10 @@ boolean pocetnapripada, krajnjapripada;
         if (i.pocetna < j.pocetna && i.krajnja < j.krajnja && j.pocetnapripada && !i.krajnjapripada)
             return new Interval(j.pocetna, i.krajnja, true, false);
         return j;
+    }
+
+    public boolean equals(Interval i){
+        if(this.pocetna == i.pocetna && this.krajnja == i.krajnja && this.pocetnapripada == i.pocetnapripada && this.krajnjapripada == i.krajnjapripada) return true;
+        return false;
     }
 }
